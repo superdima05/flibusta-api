6e428a56eb0599c7e79b4177d88dd426a27138ee
Dmitriy <superdima05@gmail.com>
2019-04-02 16:50:09 +0300

    Added HTTP GET API

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 import requests
 from bs4 import BeautifulSoup
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
 
 def search(search):
     b = 0
@@ -56,7 +59,7 @@ def search(search):
             continue
         if len(href) <= 3:
             continue
-        href_ar.append(href.encode())
+        href_ar.append(href.encode("utf-8").decode())
         b = b+1
     return href_ar
 
@@ -67,6 +70,7 @@ def get_sequence(id):
     b_ide = 0
     b_idm = 0
     x = 0
+    b_name = ""
     temp_d = []
     try:
         url = "http://flibusta.is/sequence/"+id
@@ -122,10 +126,15 @@ def get_sequence(id):
             continue
         if len(href) <= 3:
             continue
+        if "/b/" in href:
+            b_name_now = i.text.encode("utf-8").decode()
+            if b_name == "":
+                temp_d.append(b_name_now)
+                b_name = b_name_now
         if "fb2" in href:
-            temp_d.append(href)
             b_idf = href.replace("/b/", "")
             b_idf = b_idf.replace("/fb2", "")
+            temp_d.append(href)
             x = x + 1
         if "epub" in href:
             temp_d.append(href)
@@ -141,8 +150,9 @@ def get_sequence(id):
             download.append(temp_d)
             temp_d = []
             x = 0
+            b_name = ""
         if "/a/" in href and author == "":
-            author = i.text.encode("utf-8")
+            author = i.text.encode("utf-8").decode()
     result = []
     result.append(author)
     result.append(download)
@@ -150,6 +160,8 @@ def get_sequence(id):
 
 def get_book(id):
     download = []
+    b_name = ""
+    author = ""
     try:
         url = "http://flibusta.is/b/"+id
     except Exception:
@@ -210,7 +222,23 @@ def get_book(id):
             download.append(href)
         if "mobi" in href:
             download.append(href)
-    return download
+        if "/a/" in href and author == "":
+            author = i.text.encode("utf-8").decode()
+    art = soup.findAll("img", src=True)
+    cover = ""
+    for i in art:
+        if "i" in i['src']:
+            cover = i['src']
+    name = soup.find("h1", {"class": "title"}).text
+    rem_array = ["(", ")", "fb2", "mobi", "epub"]
+    for i in rem_array:
+        name = name.replace(i, "")
+    result = []
+    result.append(author)
+    result.append(name)
+    result.append(cover)
+    result.append(download)
+    return result
 
 def get_author(id):
     url = "http://flibusta.is/a/"+id
@@ -218,4 +246,45 @@ def get_author(id):
 
 search_r = search("Тургенев")
 #print(search_r)
-print(get_sequence(21476))
+#print(get_sequence(21476))
+
+def apicommands(path):
+	try:
+		parms = path.split("?")[1].split("&")
+	except Exception:
+		parms = {}
+	if "search" in path:
+		search_q = parms[0].split("=")[1]
+		search_r = search(search_q)
+		search_r = json.dumps(search_r)
+		return search_r
+	if "get_sequence" in path:
+		id = parms[0].split("=")[1]
+		links = get_sequence(id)
+		links = json.dumps(links)
+		return links
+	if "get_book" in path:
+		id = parms[0].split("=")[1]
+		links = get_book(id)
+		links = json.dumps(links)
+		return links
+	
+
+port = 8900
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if('/api' in self.path):
+            self.send_response(200)
+            self.send_header("Content-type", "text/json")
+            self.end_headers()
+            self.wfile.write(apicommands(self.path).encode())
+
+myServer = HTTPServer(('0.0.0.0', port), MyServer)
+
+try:
+    myServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+myServer.server_close()
